4005db2da9338233a55a5104c67b99b5714c1bbc
Valentin Koskin <>
2022-12-02 10:53:15 +0300

    изменил реалм(до этого просто скопипастил из важного)

--- VKapp/VKapp/Services/RealmService/RealmService.swift
+++ VKapp/VKapp/Services/RealmService/RealmService.swift
@@ -4,32 +4,31 @@
 import RealmSwift
 
 /// Service Realm
-class RealmService {
+final class RealmService {
     // MARK: - Public Properties
-
+    
     static let deleteIfMigration = Realm.Configuration(deleteRealmIfMigrationNeeded: true)
-
+    
     // MARK: - Public Methods
-
+    
     static func save<T: Object>(
         items: [T],
         config: Realm.Configuration = Realm.Configuration(deleteRealmIfMigrationNeeded: true),
         update: Bool = true
     ) {
-        print(config.fileURL ?? "")
-
+        
         do {
             let realm = try Realm(configuration: deleteIfMigration)
-
+            
             try realm.write {
                 realm.add(items, update: .modified)
             }
-
+            
         } catch {
-            print(error)
+            print(error.localizedDescription)
         }
     }
-
+    
     static func get<T: Object>(
         _ type: T.Type,
         config: Realm.Configuration = Realm.Configuration.defaultConfiguration
@@ -38,7 +37,7 @@ class RealmService {
             let realm = try Realm(configuration: deleteIfMigration)
             return realm.objects(type)
         } catch {
-            print(error)
+            print(error.localizedDescription)
         }
         return nil
     }
